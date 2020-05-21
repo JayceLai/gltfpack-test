fa64651a57bc6a95ce6858ead07eaf1c082ebe50
JayceLai <goodlaijiaxin@qq.com>
2020-05-21 16:04:04 +0800

    add more models

--- test.sh
+++ test.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-fold=bac_mono;
+# 文件夹
+fold=models/bac_mono;
 if [ $1 ]; then
 fold=$1;
 fi
 
+# 文件
 file=scene.gltf;
 if [ $2 ]; then
 file=$2;
 fi
 
+# 输出
 output=out.gltf;
 if [ $3 ]; then
 output=$3;
 fi
 
 
-echo "npx gltfpack -i $fold/$file -o $fold/$output -noq -cf -v";
+echo "npx gltfpack -i $fold/$file -o $fold/$output -noq -v";
 
 # npx gltfpack -i $fold/$file -o $fold/$output -noq -cf -v
 npx gltfpack -i $fold/$file -o $fold/$output -noq -v
